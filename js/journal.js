118db149258c92761e468c4e2830c9fb2640f508
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2020-05-09 10:42:01 +0200

    WTF homework: add aside to journal post with innerHTML

--- js/journal.js
+++ js/journal.js
@@ -4,3 +4,18 @@ let city = `Kraków`;
 console.log(
   `Witaj na mojej stronie! Nazywam się ${name}. Obecnie mieszkam w mieście: ${city}. Pozdrawiam!`
 );
+
+let titbit = document.querySelector(".journal-post__titbit-wrapper");
+
+let titbitContent = `<aside class="journal-post__titbit">
+<h3 class="journal-post__titbit-title">
+Ciekawostka
+</h3>
+<p class="journal-post__titbit-text">Chińskie pokazywanie cyfr od 1 do 10 na palcach jednej ręki</p>
+<img
+  src="/images/china/teahouse/chinese-yellow.jpg"
+  class="journal-post__titbit-image"
+/>
+</aside>`;
+
+titbit.innerHTML = titbitContent;
