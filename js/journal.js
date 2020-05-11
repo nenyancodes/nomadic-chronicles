95fa7886057c000c56be1f1373af609c89091681
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2020-05-11 06:13:07 +0200

    Polish journal tidbit inserted with JS

--- js/journal.js
+++ js/journal.js
@@ -5,16 +5,16 @@ console.log(
   `Witaj na mojej stronie! Nazywam się ${name}. Obecnie mieszkam w mieście: ${city}. Pozdrawiam!`
 );
 
-let titbit = document.querySelector(".journal-post__titbit-wrapper");
+let titbit = document.querySelector(".journal__titbit-wrapper--js");
 
-let titbitContent = `<aside class="journal-post__titbit">
-<h3 class="journal-post__titbit-title">
-Ciekawostka
+let titbitContent = `<aside class="journal__titbit">
+<h3 class="journal__titbit__title">
+Ciekawostka (inserted with JS)
 </h3>
-<p class="journal-post__titbit-text">Chińskie pokazywanie cyfr od 1 do 10 na palcach jednej ręki</p>
+<p class="journal__titbit__text">Chińskie pokazywanie cyfr od 1 do 10 na palcach jednej ręki</p>
 <img
   src="/images/china/teahouse/chinese-yellow.jpg"
-  class="journal-post__titbit-image"
+  class="journal__titbit__image"
 />
 </aside>`;
 
