3043bbda73d43e23d0f615004586f744c0ceea19
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2020-05-13 06:54:31 +0200

    Change callout's font-size, modify calloutContent

--- js/journal.js
+++ js/journal.js
@@ -7,16 +7,16 @@ let calloutContent = `
    </div>
    <div class="callout__wrapper">
       <h3 class="callout__title">
-Ciekawostka (inserted with JS)
-</h3>
+         Ciekawostka (inserted with JS)
+      </h3>
       <p class="callout__description">
-      W Chinach powszechne jest liczenie do dziesięciu używając palców jednej dłoni. Chińczycy mają własny system liczenia na palcach, dlatego mogą nas nie zrozumieć, jeżeli będziemy pokazywać cyfry po swojemu. Na przykład, mając na myśli „dwa”, nieświadomie możemy pokazać chińskie „osiem”.
+      W Chinach powszechne jest liczenie do dziesięciu używając palców jednej dłoni. Chińczycy mają własny system liczenia na palcach, dlatego mogą nas nie zrozumieć, jeżeli będziemy pokazywać cyfry po swojemu. Na przykład, mając na myśli „dwa”, nieświadomie możemy pokazać chińskie „osiem”. Jednak tak jak język chiński różni się w zależności od regionu, tak samo liczenie na dłoni również może wykazywać różnice. Liczba dziesięć pokazywana jest na trzy różne sposoby.
       </p>
     </div>
-<img
+    <img
   src="/images/china/teahouse/chinese-yellow2.jpg"
   class="callout__image"
-/>
+  />
 `;
 
 callout.innerHTML = calloutContent;
