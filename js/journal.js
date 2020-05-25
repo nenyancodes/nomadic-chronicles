21f47ccd1ab3e95457aa8cb31bdc6403cff4f346
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2020-05-25 20:20:32 +0200

    Remove greetUser function

--- js/journal.js
+++ js/journal.js
@@ -21,18 +21,18 @@ let calloutContent = `
 
 callout.innerHTML = calloutContent;
 
-const greetUser = () => {
-  const userName = prompt(`Hi! What's your name?`);
+// const greetUser = () => {
+//   const userName = prompt(`Hi! What's your name?`);
 
-  if (userName == "X Æ A-12") {
-    alert(
-      `Nice to see you, ${userName}! You've got the weirdest name on Earth! Now... be so kind and SEND ME A TESLA!!`
-    );
-  } else if (userName) {
-    alert(`Nice to see you, ${userName}!`);
-  } else {
-    alert(`Nice to see you, nameless/lazy human!`);
-  }
-};
+//   if (userName == "X Æ A-12") {
+//     alert(
+//       `Nice to see you, ${userName}! You've got the weirdest name on Earth! Now... be so kind and SEND ME A TESLA!!`
+//     );
+//   } else if (userName) {
+//     alert(`Nice to see you, ${userName}!`);
+//   } else {
+//     alert(`Nice to see you, nameless/lazy human!`);
+//   }
+// };
 
-greetUser();
+// greetUser();
