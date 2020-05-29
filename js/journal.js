d572d5f6065bab5c0bfc30be221a3d3f46a044d1
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2020-05-29 21:24:47 +0200

    WIP media queries: styling callout

--- js/journal.js
+++ js/journal.js
@@ -7,7 +7,7 @@ let calloutContent = `
    </div>
    <div class="callout__wrapper">
       <h3 class="callout__title">
-         Ciekawostka (inserted with JS)
+         Ciekawostka
       </h3>
       <p class="callout__description">
       W Chinach powszechne jest liczenie do dziesięciu używając palców jednej dłoni. Chińczycy mają własny system liczenia na palcach, dlatego mogą nas nie zrozumieć, jeżeli będziemy pokazywać cyfry po swojemu. Na przykład, mając na myśli „dwa”, nieświadomie możemy pokazać chińskie „osiem”. Jednak tak jak język chiński różni się w zależności od regionu, tak samo liczenie na dłoni również może wykazywać różnice. Liczba dziesięć pokazywana jest na trzy różne sposoby.
