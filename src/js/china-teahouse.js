aca0b69bc96dc701b2aff2956f78e690b2d7c255
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2020-06-18 13:34:33 +0200

    Webpack: fix JS callout insertion

--- src/js/china-teahouse.js
+++ src/js/china-teahouse.js
@@ -1,3 +1,27 @@
+import chineseCountingImg from "../assets/img/china/chinese-counting.jpg";
+import eurekaIcon from "../assets/icons/eureka.png";
+import eureka2xIcon from "../assets/icons/eureka-2x.png";
+
+console.log("hello another");
+let callout = document.querySelector(".callout--js");
+
+let calloutContent = `
+   <div>
+      <img src=${eurekaIcon} alt="icon" class="callout__icon"
+      srcset="${eureka2xIcon} 2x">
+   </div>
+   <div class="callout__wrapper">
+      <h3 class="callout__title">
+         Ciekawostka
+      </h3>
+      <p class="callout__description">
+      W Chinach powszechne jest liczenie do dziesięciu używając palców jednej dłoni. Chińczycy mają własny system liczenia na palcach, dlatego mogą nas nie zrozumieć, jeżeli będziemy pokazywać cyfry po swojemu. Na przykład, mając na myśli „dwa”, nieświadomie możemy pokazać chińskie „osiem”. Jednak tak jak język chiński różni się w zależności od regionu, tak samo liczenie na dłoni również może wykazywać różnice. Liczba dziesięć pokazywana jest na trzy różne sposoby.
+      </p>
+    </div>
+    <img src=${chineseCountingImg} class="callout__image" />
+`;
+
+callout.innerHTML = calloutContent;
 
 const hamburger = document.querySelector(".hamburger");
 
